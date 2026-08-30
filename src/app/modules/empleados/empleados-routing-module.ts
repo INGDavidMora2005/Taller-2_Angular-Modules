a5b316deb4d61f68f87518384e06067e4f78a85e
David Mora Duque <david.mora02@uceva.edu.co>
2026-08-30 17:49:44 -0500

    feat: aplicar estilos consistentes a tabla de empleados

--- src/app/modules/empleados/empleados-routing-module.ts
+++ src/app/modules/empleados/empleados-routing-module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmpleadosPage } from './pages/empleados-page/empleados-page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EmpleadosPage }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
