80eb900a2ba4501751035c4ca591353a3930ae6a
Juan Diego Rodriguez <juan.rodriguez16@uceva.edu.co>
2026-09-01 09:17:18 -0500

    feat: integrar modulo productos en navbar

--- src/app/core/config/navbar.config.ts
+++ src/app/core/config/navbar.config.ts
@@ -15,6 +15,6 @@ import { NavbarItem } from "../../modules/shared/interfaces/navbar.interface";
  */
 export const NAVBAR_ITEMS: NavbarItem[] = [
     { label: 'Usuarios', url: '/users' },
-    { label: 'Productos', url: '/products' },
+    { label: 'Productos', url: '/productos' },
     { label: 'Empleados', url: '/empleados' },
 ] 
