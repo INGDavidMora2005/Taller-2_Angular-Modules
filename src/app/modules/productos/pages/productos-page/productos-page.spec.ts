d95feecd6ac103d3c92c7e8e2b136c80abb66448
Juan Diego Rodriguez <juan.rodriguez16@uceva.edu.co>
2026-09-01 18:02:52 -0500

    test: corregir declaraciones de BadgeComponent en specs de productos

--- src/app/modules/productos/pages/productos-page/productos-page.spec.ts
+++ src/app/modules/productos/pages/productos-page/productos-page.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductosPage } from './productos-page';
 import { TableProductos } from '../../components/table-productos/table-productos';
+import { BadgeComponent } from '../../../shared/components/badge/badge.component';
 
 describe('ProductosPage', () => {
   let component: ProductosPage;
@@ -9,7 +10,7 @@ describe('ProductosPage', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ProductosPage, TableProductos]
+      declarations: [ProductosPage, TableProductos, BadgeComponent]
     })
     .compileComponents();
 
