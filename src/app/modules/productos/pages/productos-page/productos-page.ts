66f423e0375da0c2a872f74a133754899edd3958
Juan Diego Rodriguez <juan.rodriguez16@uceva.edu.co>
2026-08-31 19:49:05 -0500

    feat: crear pagina principal de productos

---
+++ src/app/modules/productos/pages/productos-page/productos-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos-page',
+  standalone: false,
+  templateUrl: './productos-page.html',
+  styleUrl: './productos-page.scss',
+})
+export class ProductosPage {
+
+}
--- src/app/modules/productos/productos-module.ts
+++ src/app/modules/productos/productos-module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProductosRoutingModule } from './productos-routing-module';
 import { TableProductos } from './components/table-productos/table-productos';
+import { ProductosPage } from './pages/productos-page/productos-page';
 
 
 @NgModule({
   declarations: [
-    TableProductos
+    TableProductos,
+    ProductosPage
   ],
   imports: [
     CommonModule,
