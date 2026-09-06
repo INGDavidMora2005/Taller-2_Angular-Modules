3d69ce9d43fbb5e80f2bb59874a52d1e02e61100
David Mora Duque <david.mora02@uceva.edu.co>
2026-09-06 14:57:17 -0500

    test: agregar pruebas unitarias para servicio de cursos

--- src/app/modules/cursos/services/cursos.spec.ts
+++ src/app/modules/cursos/services/cursos.spec.ts
@@ -13,4 +13,19 @@ describe('Cursos', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('getCursos() debería retornar un arreglo', () => {
+    const cursos = service.getCursos();
+    expect(Array.isArray(cursos)).toBe(true);
+  });
+
+  it('getCursos() debería retornar exactamente 6 cursos', () => {
+    const cursos = service.getCursos();
+    expect(cursos).toHaveLength(6);
+  });
+
+  it('el primer curso debería tener el nombre "Arquitectura de Software"', () => {
+    const cursos = service.getCursos();
+    expect(cursos[0].nombre).toBe('Arquitectura de Software');
+  });
 });
