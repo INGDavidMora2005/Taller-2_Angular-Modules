934a89d2a794ceca2e40f9e11e40efa2a044c071
David Mora Duque <david.mora02@uceva.edu.co>
2026-09-06 12:45:23 -0500

    feat: crear pagina principal de cursos

--- src/app/modules/cursos/cursos-module.ts
+++ src/app/modules/cursos/cursos-module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CursosRoutingModule } from './cursos-routing-module';
 import { TableCursos } from './components/table-cursos/table-cursos';
+import { CursosPage } from './pages/cursos-page/cursos-page';
 
 
 @NgModule({
   declarations: [
-    TableCursos
+    TableCursos,
+    CursosPage
   ],
   imports: [
     CommonModule,
